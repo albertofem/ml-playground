08f681bb757cb5810496b664b5e7b26f40402b90
Alberto Fernández <albertofem@gmail.com>
2023-03-01 23:30:41 +0100

    Added torchserve initial stuff, add inference code

--- training/train.py
+++ training/train.py
@@ -5,130 +5,20 @@ import torch.nn as nn
 from sklearn.model_selection import train_test_split
 from transformers import AutoModel, BertTokenizerFast
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
-
+from sklearn.utils.class_weight import compute_class_weight
 import argparse
 
 parser = argparse.ArgumentParser(description='Argument Parser')
 
-parser.add_argument('--training_data', type=str, required=True, help='Location of the training data')
-parser.add_argument('--model_output_directory', type=str, required=True, help='Directory to output the model')
+parser.add_argument('--training_data_file', type=str, required=True, help='Location of the training data')
+parser.add_argument('--model_output_dir', type=str, required=True, help='Directory to output the model')
 
 args = parser.parse_args()
 
-# specify GPU
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-df = pd.read_csv(args.training_data)
-
-train_text, temp_text, train_labels, temp_labels = train_test_split(df['text'], df['label'],
-                                                                    random_state=2018,
-                                                                    test_size=0.3,
-                                                                    stratify=df['label'])
-
-val_text, test_text, val_labels, test_labels = train_test_split(temp_text, temp_labels,
-                                                                random_state=2018,
-                                                                test_size=0.5,
-                                                                stratify=temp_labels)
-
-bert = AutoModel.from_pretrained('bert-base-uncased')
-
-tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
-
-seq_len = [len(i.split()) for i in train_text]
-
-max_seq_len = 25
-
-tokens_train = tokenizer.batch_encode_plus(
-    train_text.tolist(),
-    max_length=max_seq_len,
-    padding=True,
-    truncation=True,
-    return_token_type_ids=False
-)
-
-# tokenize and encode sequences in the validation set
-tokens_val = tokenizer.batch_encode_plus(
-    val_text.tolist(),
-    max_length=max_seq_len,
-    padding=True,
-    truncation=True,
-    return_token_type_ids=False
-)
-
-# tokenize and encode sequences in the test set
-tokens_test = tokenizer.batch_encode_plus(
-    test_text.tolist(),
-    max_length=max_seq_len,
-    padding=True,
-    truncation=True,
-    return_token_type_ids=False
-)
-
-train_seq = torch.tensor(tokens_train['input_ids'])
-train_mask = torch.tensor(tokens_train['attention_mask'])
-train_y = torch.tensor(train_labels.tolist())
-
-# for validation set
-val_seq = torch.tensor(tokens_val['input_ids'])
-val_mask = torch.tensor(tokens_val['attention_mask'])
-val_y = torch.tensor(val_labels.tolist())
-
-# for test set
-test_seq = torch.tensor(tokens_test['input_ids'])
-test_mask = torch.tensor(tokens_test['attention_mask'])
-test_y = torch.tensor(test_labels.tolist())
-
-# define a batch size
-batch_size = 32
-
-# wrap tensors
-train_data = TensorDataset(train_seq, train_mask, train_y)
-
-# sampler for sampling the data during training
-train_sampler = RandomSampler(train_data)
-
-# dataLoader for train set
-train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
-
-# wrap tensors
-val_data = TensorDataset(val_seq, val_mask, val_y)
-
-# sampler for sampling the data during training
-val_sampler = SequentialSampler(val_data)
-
-# dataLoader for validation set
-val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size)
-
-# define a batch size
-batch_size = 32
-
-# wrap tensors
-train_data = TensorDataset(train_seq, train_mask, train_y)
-
-# sampler for sampling the data during training
-train_sampler = RandomSampler(train_data)
-
-# dataLoader for train set
-train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
-
-# wrap tensors
-val_data = TensorDataset(val_seq, val_mask, val_y)
-
-# sampler for sampling the data during training
-val_sampler = SequentialSampler(val_data)
-
-# dataLoader for validation set
-val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size)
-
-# freeze all the parameters
-for param in bert.parameters():
-    param.requires_grad = False
-
-
-class BERT_Arch(nn.Module):
-
+class BertSpamDetectorModel(nn.Module):
     def __init__(self, bert):
-        super(BERT_Arch, self).__init__()
+        super(BertSpamDetectorModel, self).__init__()
 
         self.bert = bert
 
@@ -167,166 +57,191 @@ class BERT_Arch(nn.Module):
         return x
 
 
-# pass the pre-trained BERT to our define architecture
-model = BERT_Arch(bert)
+class BertSpamDetectorTrainer:
+    def __init__(self):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# push the model to GPU
-model = model.to(device)
+        self.df = pd.read_csv(args.training_data)
 
-# define the optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+        self.train_text, \
+            self.temp_text, \
+            self.train_labels, \
+            self.temp_labels = train_test_split(self.df['text'], self.df['label'],
+                                                random_state=2018,
+                                                test_size=0.3,
+                                                stratify=self.df[
+                                                    'label'])
 
-from sklearn.utils.class_weight import compute_class_weight
+        self.val_text, \
+            self.test_text, \
+            self.val_labels, \
+            self.test_labels = train_test_split(self.temp_text,
+                                                self.temp_labels,
+                                                random_state=2018,
+                                                test_size=0.5,
+                                                stratify=self.temp_labels)
 
-# compute the class weights
-class_wts = compute_class_weight('balanced', classes=np.unique(train_labels), y=train_labels)
+        self.bert = AutoModel.from_pretrained('bert-base-uncased')
 
-# convert class weights to tensor
-weights = torch.tensor(class_wts, dtype=torch.float)
-weights = weights.to(device)
+        self.tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
 
-# loss function
-cross_entropy = nn.NLLLoss(weight=weights)
+        self.max_seq_len = 25
 
-# number of training epochs
-epochs = 10
+        self.tokens_train = self.tokenizer.batch_encode_plus(
+            self.train_text.tolist(),
+            max_length=self.max_seq_len,
+            padding=True,
+            truncation=True,
+            return_token_type_ids=False
+        )
 
+        self.tokens_val = self.tokenizer.batch_encode_plus(
+            self.val_text.tolist(),
+            max_length=self.max_seq_len,
+            padding=True,
+            truncation=True,
+            return_token_type_ids=False
+        )
 
-def train():
-    model.train()
+        self.tokens_test = self.tokenizer.batch_encode_plus(
+            self.test_text.tolist(),
+            max_length=self.max_seq_len,
+            padding=True,
+            truncation=True,
+            return_token_type_ids=False
+        )
 
-    total_loss, total_accuracy = 0, 0
+        self.train_seq = torch.tensor(self.tokens_train['input_ids'])
+        self.train_mask = torch.tensor(self.tokens_train['attention_mask'])
+        self.train_y = torch.tensor(self.train_labels.tolist())
 
-    # empty list to save model predictions
-    total_preds = []
+        self.val_seq = torch.tensor(self.tokens_val['input_ids'])
+        self.val_mask = torch.tensor(self.tokens_val['attention_mask'])
+        self.val_y = torch.tensor(self.val_labels.tolist())
 
-    # iterate over batches
-    for step, batch in enumerate(train_dataloader):
+        self.test_seq = torch.tensor(self.tokens_test['input_ids'])
+        self.test_mask = torch.tensor(self.tokens_test['attention_mask'])
+        self.test_y = torch.tensor(self.test_labels.tolist())
 
-        # progress update after every 50 batches.
-        if step % 50 == 0 and not step == 0:
-            print('  Batch {:>5,}  of  {:>5,}.'.format(step, len(train_dataloader)))
+        self.batch_size = 32
 
-        # push the batch to gpu
-        batch = [r.to(device) for r in batch]
+        self.train_data = TensorDataset(self.train_seq, self.train_mask, self.train_y)
 
-        sent_id, mask, labels = batch
+        self.train_sampler = RandomSampler(self.train_data)
 
-        # clear previously calculated gradients
-        model.zero_grad()
+        self.train_dataloader = DataLoader(self.train_data, sampler=self.train_sampler, batch_size=self.batch_size)
 
-        # get model predictions for the current batch
-        preds = model(sent_id, mask)
+        self.val_data = TensorDataset(self.val_seq, self.val_mask, self.val_y)
 
-        # compute the loss between actual and predicted values
-        loss = cross_entropy(preds, labels)
+        self.val_sampler = SequentialSampler(self.val_data)
 
-        # add on to the total loss
-        total_loss = total_loss + loss.item()
+        self.val_dataloader = DataLoader(self.val_data, sampler=self.val_sampler, batch_size=self.batch_size)
 
-        # backward pass to calculate the gradients
-        loss.backward()
+        for param in self.bert.parameters():
+            param.requires_grad = False
 
-        # clip the the gradients to 1.0. It helps in preventing the exploding gradient problem
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        self.model = BertSpamDetectorModel(self.bert)
 
-        # update parameters
-        optimizer.step()
+        self.model = self.model.to(self.device)
 
-        # model predictions are stored on GPU. So, push it to CPU
-        preds = preds.detach().cpu().numpy()
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-3)
 
-        # append the model predictions
-        total_preds.append(preds)
+        self.class_wts = compute_class_weight('balanced', classes=np.unique(self.train_labels), y=self.train_labels)
 
-    # compute the training loss of the epoch
-    avg_loss = total_loss / len(train_dataloader)
+        self.weights = torch.tensor(self.class_wts, dtype=torch.float)
+        self.weights = self.weights.to(self.device)
 
-    # predictions are in the form of (no. of batches, size of batch, no. of classes).
-    # reshape the predictions in form of (number of samples, no. of classes)
-    total_preds = np.concatenate(total_preds, axis=0)
+        self.cross_entropy = nn.NLLLoss(weight=self.weights)
 
-    # returns the loss and predictions
-    return avg_loss, total_preds
+        self.epochs = 10
 
+    def train(self):
+        self.model.train()
 
-def evaluate():
-    print("\nEvaluating...")
+        total_loss, total_accuracy = 0, 0
 
-    # deactivate dropout layers
-    model.eval()
+        total_preds = []
 
-    total_loss, total_accuracy = 0, 0
+        for step, batch in enumerate(self.train_dataloader):
+            if step % 50 == 0 and not step == 0:
+                print('  Batch {:>5,}  of  {:>5,}.'.format(step, len(self.train_dataloader)))
 
-    # empty list to save the model predictions
-    total_preds = []
+            batch = [r.to(self.device) for r in batch]
 
-    # iterate over batches
-    for step, batch in enumerate(val_dataloader):
+            sent_id, mask, labels = batch
 
-        # Progress update every 50 batches.
-        if step % 50 == 0 and not step == 0:
-            # Calculate elapsed time in minutes.
-            elapsed = format_time(time.time() - t0)
+            self.model.zero_grad()
 
-            # Report progress.
-            print('  Batch {:>5,}  of  {:>5,}.'.format(step, len(val_dataloader)))
-
-        # push the batch to gpu
-        batch = [t.to(device) for t in batch]
-
-        sent_id, mask, labels = batch
-
-        # deactivate autograd
-        with torch.no_grad():
-
-            # model predictions
-            preds = model(sent_id, mask)
-
-            # compute the validation loss between actual and predicted values
-            loss = cross_entropy(preds, labels)
-
+            preds = self.model(sent_id, mask)
+            loss = self.cross_entropy(preds, labels)
             total_loss = total_loss + loss.item()
+
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+
+            self.optimizer.step()
 
             preds = preds.detach().cpu().numpy()
 
             total_preds.append(preds)
 
-    # compute the validation loss of the epoch
-    avg_loss = total_loss / len(val_dataloader)
+        avg_loss = total_loss / len(self.train_dataloader)
 
-    # reshape the predictions in form of (number of samples, no. of classes)
-    total_preds = np.concatenate(total_preds, axis=0)
+        total_preds = np.concatenate(total_preds, axis=0)
 
-    return avg_loss, total_preds
+        return avg_loss, total_preds
 
+    def evaluate(self):
+        print("\nEvaluating...")
 
-# set initial loss to infinite
-best_valid_loss = float('inf')
+        self.model.eval()
 
-# empty lists to store training and validation loss of each epoch
-train_losses = []
-valid_losses = []
+        total_loss, total_accuracy = 0, 0
+        total_preds = []
 
-# for each epoch
-for epoch in range(epochs):
+        for step, batch in enumerate(self.val_dataloader):
+            if step % 50 == 0 and not step == 0:
+                print('Batch {:>5,}  of  {:>5,}.'.format(step, len(self.val_dataloader)))
 
-    print('\n Epoch {:} / {:}'.format(epoch + 1, epochs))
+            batch = [t.to(self.device) for t in batch]
 
-    # train model
-    train_loss, _ = train()
+            sent_id, mask, labels = batch
 
-    # evaluate model
-    valid_loss, _ = evaluate()
+            with torch.no_grad():
+                preds = self.model(sent_id, mask)
+                loss = self.cross_entropy(preds, labels)
+                total_loss = total_loss + loss.item()
 
-    # save the best model
-    if valid_loss < best_valid_loss:
-        best_valid_loss = valid_loss
-        torch.save(model.state_dict(), '{}/saved_weights.pt'.format(args.model_output_directory))
+                preds = preds.detach().cpu().numpy()
+                total_preds.append(preds)
 
-    # append training and validation loss
-    train_losses.append(train_loss)
-    valid_losses.append(valid_loss)
+        avg_loss = total_loss / len(self.val_dataloader)
 
-    print(f'\nTraining Loss: {train_loss:.3f}')
-    print(f'Validation Loss: {valid_loss:.3f}')
+        total_preds = np.concatenate(total_preds, axis=0)
+
+        return avg_loss, total_preds
+
+    def run(self):
+        best_valid_loss = float('inf')
+
+        train_losses = []
+        valid_losses = []
+
+        # for each epoch
+        for epoch in range(self.epochs):
+
+            print('\n Epoch {:} / {:}'.format(epoch + 1, self.epochs))
+
+            train_loss, _ = self.train()
+            valid_loss, _ = self.evaluate()
+
+            if valid_loss < best_valid_loss:
+                best_valid_loss = valid_loss
+                torch.save(self.model.state_dict(), '{}/saved_weights.pt'.format(args.model_output_directory))
+
+            train_losses.append(train_loss)
+            valid_losses.append(valid_loss)
+
+            print(f'\nTraining Loss: {train_loss:.3f}')
+            print(f'Validation Loss: {valid_loss:.3f}')
